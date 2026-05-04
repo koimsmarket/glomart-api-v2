9c83436fa2539428c3f46446706a35389133c8df
koimsmarket <koims.market@gmail.com>
2026-05-05 01:34:05 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Glomart API running');
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Glomart API running on ${PORT}`);
+});
