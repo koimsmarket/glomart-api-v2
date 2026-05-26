81e1e9580fa927b65b4022a0b0f724a6937e151b
koimsmarket <koims.market@gmail.com>
2026-05-26 12:47:47 +0900

    remove olCommit 1 file to maind example file

--- index_add_example.js
+++
@@ -1,8 +0,0 @@
-app.locals.pool = pool;
-app.use(express.json({ limit: '2mb' }));
-
-app.use(require('./routes/health'));
-app.use(require('./routes/product'));
-app.use(require('./routes/cart'));
-app.use(require('./routes/order'));
-app.use(require('./routes/cs'));
